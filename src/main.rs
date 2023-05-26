eb0f5d429aeeeffd6028cf5f1fb31841ab95f8b2
hatoo <hato2000@gmail.com>
2023-05-26 22:11:00 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -11,24 +11,28 @@ fn main() -> Result<()> {
         _ => {}
     }
 
+    let mut file = File::open(&args[1])?;
+    let mut header = [0; 100];
+    file.read_exact(&mut header)?;
+
+    // The page size is stored at the 16th byte offset, using 2 bytes in big-endian order
+    let page_size = u16::from_be_bytes([header[16], header[17]]);
+
+    let mut first_page = vec![0; page_size as usize - 100];
+    file.read_exact(&mut first_page)?;
+    let first_page = first_page;
+
+    let number_of_cells = u16::from_be_bytes([first_page[3], first_page[4]]);
+
     // Parse command and act accordingly
     let command = &args[2];
     match command.as_str() {
         ".dbinfo" => {
-            let mut file = File::open(&args[1])?;
-            let mut header = [0; 100];
-            file.read_exact(&mut header)?;
-
-            // The page size is stored at the 16th byte offset, using 2 bytes in big-endian order
-            let page_size = u16::from_be_bytes([header[16], header[17]]);
-
             println!("database page size: {}", page_size);
-
-            let mut first_page = vec![0; page_size as usize - 100];
-            file.read_exact(&mut first_page)?;
-
-            let number_of_cells = u16::from_be_bytes([first_page[3], first_page[4]]);
             println!("number of tables: {}", number_of_cells);
+        }
+        ".tables" => {
+            todo!()
         }
         _ => bail!("Missing or invalid command passed: {}", command),
     }
