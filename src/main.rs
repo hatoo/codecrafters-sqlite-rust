48906822e20c0ff8533476b0e9fbd5c025de8f58
hatoo <hato2000@gmail.com>
2023-05-26 22:48:06 +0900

    WIP

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,20 @@ use anyhow::{bail, Result};
 use std::fs::File;
 use std::io::prelude::*;
 
+fn variant(buf: &[u8]) -> (u64, &[u8]) {
+    let mut i = 0;
+    let mut v = 0;
+    loop {
+        let byte = buf[i];
+        v = (v << 7) | (byte & 0x7f) as u64;
+        if byte & 0x80 == 0 {
+            break;
+        }
+        i += 1;
+    }
+    (v, &buf[i + 1..])
+}
+
 fn main() -> Result<()> {
     // Parse arguments
     let args = std::env::args().collect::<Vec<_>>();
@@ -32,7 +46,18 @@ fn main() -> Result<()> {
             println!("number of tables: {}", number_of_cells);
         }
         ".tables" => {
-            todo!()
+            assert_eq!(first_page[0], 0x0d);
+            let cell_indices = (0..number_of_cells as usize)
+                .map(|i| u16::from_be_bytes([first_page[8 + 2 * i], first_page[8 + 2 * i + 1]]))
+                .collect::<Vec<_>>();
+
+            for i in cell_indices {
+                let cell = &first_page[i as usize..];
+                let (payload_length, cell) = variant(cell);
+                let (row_id, cell) = variant(cell);
+
+                println!("length {payload_length}, row_id: {row_id}");
+            }
         }
         _ => bail!("Missing or invalid command passed: {}", command),
     }
