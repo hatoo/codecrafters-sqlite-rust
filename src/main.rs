d01d23dfeea4f53f0594fc82e0a38cb47057258c
hatoo <hato2000@gmail.com>
2023-05-27 15:27:20 +0900

    WIP

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,14 @@ use anyhow::{bail, Result};
 use std::fs::File;
 use std::io::{prelude::*, SeekFrom};
 
+struct Table {
+    ty: String,
+    name: String,
+    tbl_name: String,
+    rootpage: u32,
+    sql: String,
+}
+
 fn variant(buf: &[u8]) -> (u64, &[u8]) {
     let mut i = 0;
     let mut v = 0;
@@ -14,6 +22,69 @@ fn variant(buf: &[u8]) -> (u64, &[u8]) {
         i += 1;
     }
     (v, &buf[i + 1..])
+}
+
+fn tables(first_page: &[u8]) -> Vec<Table> {
+    assert_eq!(first_page[100], 0x0d);
+    let number_of_cells = u16::from_be_bytes([first_page[103], first_page[104]]);
+
+    let cell_indices = (0..number_of_cells as usize)
+        .map(|i| u16::from_be_bytes([first_page[100 + 8 + 2 * i], first_page[100 + 8 + 2 * i + 1]]))
+        .collect::<Vec<_>>();
+
+    cell_indices
+        .into_iter()
+        .map(|i| {
+            let cell = &first_page[i as usize..];
+            let (_payload_length, cell) = variant(cell);
+            let (_row_id, cell) = variant(cell);
+            // assume header length is 1 byte
+            let header_length = cell[0];
+            let mut header = &cell[1..header_length as usize];
+            let mut cell = &cell[header_length as usize..];
+
+            // type text
+            let (t, header) = variant(header);
+            assert!(t >= 13 && t % 2 == 1);
+            let length = ((t - 13) / 2) as usize;
+            let ty = std::str::from_utf8(&cell[..length]).unwrap();
+            cell = &cell[length..];
+
+            // name text
+            let (t, header) = variant(header);
+            assert!(t >= 13 && t % 2 == 1);
+            let length = ((t - 13) / 2) as usize;
+            let name = std::str::from_utf8(&cell[..length]).unwrap();
+            cell = &cell[length..];
+
+            // tbl_name text
+            let (t, header) = variant(header);
+            assert!(t >= 13 && t % 2 == 1);
+            let length = ((t - 13) / 2) as usize;
+            let tbl_name = std::str::from_utf8(&cell[..length]).unwrap();
+            cell = &cell[length..];
+
+            // rootpage integer
+            let (t, header) = variant(header);
+            assert_eq!(t, 1);
+            let rootpage = cell[0] as u32;
+            cell = &cell[1..];
+
+            // sql text
+            let (t, _header) = variant(header);
+            assert!(t >= 13 && t % 2 == 1);
+            let length = ((t - 13) / 2) as usize;
+            let sql = std::str::from_utf8(&cell[..length]).unwrap();
+
+            Table {
+                ty: ty.to_string(),
+                name: name.to_string(),
+                tbl_name: tbl_name.to_string(),
+                rootpage,
+                sql: sql.to_string(),
+            }
+        })
+        .collect()
 }
 
 fn main() -> Result<()> {
@@ -46,50 +117,21 @@ fn main() -> Result<()> {
             println!("number of tables: {}", number_of_cells);
         }
         ".tables" => {
-            assert_eq!(first_page[0], 0x0d);
-            let cell_indices = (0..number_of_cells as usize)
-                .map(|i| u16::from_be_bytes([first_page[8 + 2 * i], first_page[8 + 2 * i + 1]]))
-                .collect::<Vec<_>>();
-
             let mut first_page = vec![0; page_size as usize];
             file.seek(SeekFrom::Start(0))?;
             file.read_exact(&mut first_page)?;
             let first_page = first_page;
 
-            let mut table_names = vec![];
-
-            for i in cell_indices {
-                let cell = &first_page[i as usize..];
-                let (_payload_length, cell) = variant(cell);
-                let (_row_id, cell) = variant(cell);
-                // assume header length is 1 byte
-                let header_length = cell[0];
-                let mut header = &cell[1..header_length as usize];
-                let mut cell = &cell[header_length as usize..];
-
-                let mut i = 0;
-
-                while !header.is_empty() {
-                    let (t, header_) = variant(header);
-                    header = header_;
-                    match t {
-                        1 => {
-                            cell = &cell[1..];
-                        }
-                        t => {
-                            assert!(t >= 13);
-                            let length = ((t - 13) / 2) as usize;
-                            let text = std::str::from_utf8(&cell[..length]).unwrap();
-                            if i == 1 && text != "sqlite_sequence" {
-                                table_names.push(text);
-                            }
-                            cell = &cell[length..];
-                        }
-                    }
-                    i += 1;
-                }
-            }
-            println!("{}", table_names.join(" "));
+            let tables = tables(&first_page);
+            println!(
+                "{}",
+                tables
+                    .into_iter()
+                    .filter(|t| t.name != "sqlite_sequence")
+                    .map(|t| t.name)
+                    .collect::<Vec<_>>()
+                    .join(" ")
+            );
         }
         _ => bail!("Missing or invalid command passed: {}", command),
     }
